5f884ac4927c271232d6d42c43f25a8f4ba98519
LazaroDeTormes <paladinalex470@gmail.com>
2023-01-19 11:28:42 +0100

    pedidos

--- models/informacion.py
+++ models/informacion.py
@@ -67,3 +67,6 @@ class informacion(models.Model):
         for rexistro in self:
             if rexistro.peso < 1 or rexistro.peso > 10:
                 raise ValidationError('Os peso de %s ten que ser entre 1 e 4 ' % rexistro.name)
+
+    def _cambia_campo_sexo(self, rexistro):
+        rexistro.sexo_traducido = "Hombre"
---
+++ models/lineapedido.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class linerapedido(models.Model):
+    _name = 'odoo_basico.pedido'
+    _description = 'Ejemplo de pedido'
+
+    name = fields.Char(required=True, size=20, string = "Línea de pedido")
+
+    descripcion_lp = fields.Text(string="Descripción de la línea de pedido")
+
+    pedido_id = fields.Many2one('odoo_basico.pedido', ondelete="cascade", required=True)
+
+    informacion_ids = fields.Many2many('odoo_basico.informacion',
+                                       string="Registro de información",
+                                       relation="odoo_basido_lineapedido_informacion",
+                                       column1="lineapedido_id", column2="informacion_id")
+
